9bda82b7460c7c37ad1c5df191dcdd5e9d6aeee7
Jorge Root <lassancejorgeluiz@gmail.com>
2021-09-30 19:27:31 -0300

    Montando Migrations

---
+++ src/database/migrations/2021_09_30_181300_create_all_tables.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAllTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->char('sexo', 9);
+            $table->integer('idade');
+            $table->string('hobby', 100);
+            $table->date('datanascimento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devs');
+    }
+}
